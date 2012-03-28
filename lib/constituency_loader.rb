83c1022c6bd04f298fe100841cc5fc45e0961187
lizconlan <lizconlan@gmail.com>
2012-03-28 01:30:30 +0100

    better constituency id - removed spaces, lower case

--- lib/constituency_loader.rb
+++ lib/constituency_loader.rb
@@ -4,15 +4,6 @@ require 'date'
 require_relative '../models/constituency.rb'
 
 class ConstituencyLoader
-  #shouldn't be here - sloppy
-  MONGO_URL = ENV['MONGOHQ_URL'] || YAML::load(File.read("config/mongo.yml"))[:mongohq_url]
-  
-  def initialize
-    env = {}
-    MongoMapper.config = { env => {'uri' => MONGO_URL} }
-    MongoMapper.connect(env)
-  end
-    
   def load_from_scraperwiki(scraper_name="parly_constituencies")
     url = "https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name=#{scraper_name}&query=select%20*%20from%20%60swdata%60"
     response = RestClient.get(url)
@@ -24,7 +15,7 @@ class ConstituencyLoader
       if record["abolished"]
         constituency.year_abolished = record["abolished"]
       end
-      constituency.id = "#{constituency.name}_#{constituency.year_created}"
+      constituency.id = "#{constituency.name.downcase().gsub(" ","-")}_#{constituency.year_created}"
       constituency.save
     end
   end
