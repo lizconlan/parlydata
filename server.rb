f26369c53b332bf28ea0c8e2f7f6314bce71a511
lizconlan <lizconlan@gmail.com>
2012-04-03 12:44:11 +0100

    basic API demo with Swagger documentation

---
+++ server.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'mongo_mapper'
+
+MONGO_URL = ENV['MONGOHQ_URL'] || YAML::load(File.read("config/mongo.yml"))[:mongohq_url]
+env = {}
+MongoMapper.config = { env => {'uri' => MONGO_URL} }
+MongoMapper.connect(env)
+
+require_relative "models/constituency"
+require_relative "models/timeline_element"
+
+get "/" do
+  "yo"
+end
+
+get "/api/?" do
+  File.read(File.join('public/api', 'index.html'))
+end
+
+get "/api/constituency/detail.json?" do
+  name = params[:name]
+  year = params[:year]
+  if name and year
+    constituencies = Constituency.find_constituency(name, year.to_i)
+  elsif name
+    constituencies = Constituency.find_all_by_name(/#{params[:name]}/i)
+  end
+  constituencies.to_json
+end
+
+get "/api/constituency.json" do
+  %|{"apis":[
+      {
+        "path":"/constituency/detail.json",
+        "description":"Constituency info",
+        "operations":[
+          {
+            "parameters":[
+              {
+                "name":"name",
+                "description":"Constituency name",
+                "dataType":"string",
+                "required":true,
+                "allowMultiple":false,
+                "paramType":"query"
+              },
+              {
+                "name":"year",
+                "description":"Year",
+                "dataType":"int",
+                "required":false,
+                "allowMultiple":false,
+                "paramType":"query"
+              }
+            ],
+            "httpMethod":"GET",
+            "notes":"Returns some stuff",
+            "responseTypeInternal":"com.parlydata.api.model.Constituency",
+            "errorResponses":[{"reason":"Invalid ID supplied","code":400},{"reason":"Pet not found","code":404}],
+            "nickname":"getDetail",
+            "responseClass":"constituency",
+            "summary":"Get constituency detail"
+          }
+        ]
+      }
+    ],
+    "models": {
+      "Constituency":{
+        "properties":{
+          "election_ids":{"type":"array","items":{"$ref":"election"}},
+          "id":{"type":"string"},
+          "name":{"type":"string"},
+          "year_abolished":{"type":"int"},
+          "year_created":{"type":"int"}
+        },
+        "id":"constituency"
+      },
+      "Pet":{"properties":{"tags":{"type":"array","items":{"$ref":"tag"}},"id":{"type":"long"},"category":{"type":"category"},"status":{"type":"string","description":"pet status in the store","allowableValues":{"values":["available","pending","sold"],"valueType":"LIST"}},"name":{"type":"string"},"photoUrls":{"type":"array","items":{"type":"string"}}},"id":"pet"},
+      "Tag":{"properties":{"id":{"type":"long"},"name":{"type":"string"}},"id":"tag"}
+    },
+    "basePath":"http://localhost:4567/api",
+    "swaggerVersion":"1.1-SHAPSHOT.121026",
+    "apiVersion":"1"}|
+end
+
+get "/api/resources.json" do
+  %|{"apis":[{"path":"/constituency.{format}","description":"Operations about constituency"}],"basePath":"http://localhost:4567/api","swaggerVersion":"1.1-SHAPSHOT.121026","apiVersion":"1"}|
+end
