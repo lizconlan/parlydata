401b77c71dd195289df288c6f219523e18bfc1b0
lizconlan <lizconlan@gmail.com>
2012-06-08 18:22:18 +0100

    starting to add missing member data

---
+++ data/members.js
@@ -0,0 +1,102 @@
+[
+    {
+        "title": "Mr",
+        "surname": "Clapham",
+        "forenames": "Mick",
+        "born": "15 May 1943"
+    },
+    {
+        "title": "Mr",
+        "surname": "Marshall",
+        "forenames": "Jim",
+        "born": "13 March 1941",
+        "died": "27 May 2004"
+    },
+    {
+        "title": "Ms",
+        "surname": "Anderson",
+        "forenames": "Janet",
+        "born": "6 December 1949"
+    },
+    {
+        "title": "Sir",
+        "surname": "Powell",
+        "forenames": "Ray",
+        "born": "19 June 1928",
+        "died": "7 December 2001"
+    },
+    {
+        "title": "Mr",
+        "surname": "Gill",
+        "forenames": "Christopher",
+        "born": "28 October 1936"
+    },
+    {
+        "title": "Mr",
+        "surname": "McKelvey",
+        "forenames": "William",
+        "born": "July 1934"
+    },
+    {
+        "title": "Mr",
+        "surname": "Parry",
+        "forenames": "Robert",
+        "born": "8 January 1933",
+        "died": "9 March 2000"
+    },
+    {
+        "title": "Sir",
+        "surname": "Mitchell",
+        "forenames": "David",
+        "born": "20 June 1928"
+    },
+    {
+        "title": "Sir",
+        "surname": "Lightbrown",
+        "forenames": "David",
+        "born": "30 November 1932",
+        "died": "12 December 1995"
+    },
+    {
+        "title": "Mr",
+        "surname": "Aspinwall",
+        "forenames": "Jack",
+        "born": "5 February 1933"
+    },
+    {
+        "title": "Mr",
+        "surname": "Herbert",
+        "forenames": "Nick",
+        "born": "7 April 1963"
+    },
+    {
+        "title": "Mr",
+        "surname": "McDonnell",
+        "forenames": "Alasdair",
+        "born": "1 September 1949"
+    },
+    {
+        "title": "Mr",
+        "surname": "Holloway",
+        "forenames": "Adam",
+        "born": "29 July 1965"
+    },
+    {
+        "title": "Ms",
+        "surname": "Milton",
+        "forenames": "Anne",
+        "born": "3 November 1955"
+    },
+    {
+        "title": "Mr",
+        "surname": "Hands",
+        "forenames": "Greg",
+        "born": "14 November 1965"
+    },
+    {
+        "title": "Mr",
+        "surname": "Wright",
+        "forenames": "Iain",
+        "born": "9 May 1972"
+    }
+]
