f2e03f9849790fc75bcd0867246c3d7940ff262e
lizconlan <lizconlan@gmail.com>
2012-06-08 18:17:41 +0100

    moved much of the finder code to the Person model

--- lib/person_loader.rb
+++ lib/person_loader.rb
@@ -15,13 +15,69 @@ class PersonLoader
       person.surname = record["surname"]
       person.born = record["born"] if record["born"] and record["born"] != ""
       person.died = record["died"] if record["died"] and record["died"] != ""
-            
+      
+      person.forenames = "John" if person.title == "Viscount" and person.surname == "Thurso"
+      
+      if person.forenames == "Michael" and person.surname == "Foster" and person.born.to_s == "1946-02-26"
+        person.forenames = "Michael Jabez"
+      end
+      
+      if person.forenames == "David" and person.surname == "Young" and person.born.to_s == "1928-10-12"
+        person.born = "1930-10-12" #some sources say 1928, Who Was Who says 1930 so switching to that
+      end
+      
+      if person.forenames == "Helen" and person.surname == "Brinton"
+        person.surname = "Clark"
+        person.add_aka("Helen Brinton")
+      end
+      if person.forenames == "Anne" and person.surname == "Picking"
+        person.surname = "Moffat"
+        person.add_aka("Ann Picking")
+      end
+      if person.forenames == "Margaret" and person.surname == "Jackson"
+        person.surname = "Beckett"
+        person.add_aka("Margaret Jackson")
+      end
+      if person.forenames == "Margaret" and person.surname == "Bain" and person.born.to_s == "1945-09-01"
+        person.add_aka("Margaret Ewing")
+        person.add_aka("Margaret McAdam")
+      end
+      if person.forenames == "John" and person.surname == "Roberts" and person.born.to_s == "1935-10-23"
+        person.add_aka("Roger Roberts")
+      end
+      if person.forenames == "John" and person.surname == "Hannam" and person.born.to_s == "1929-08-02"
+        person.add_aka("George Hannam")
+      end
+      if person.forenames == "Elaine" and person.surname == "Kellett"
+        person.add_aka("Elaine Kellett-Bowman")
+        person.add_aka("Mary Elaine Kay")
+        person.add_aka("Mary Kellett")
+        person.add_aka("Dame Mary Elaine Kellett-Bowman")
+      end
+      
       person.url = record["url"]
       if person.born and person.born.year > 1900 #restrict the dataset to recent times
         born = person.born.year.to_s
         person.id = "#{person.surname}_#{person.forenames[0..0]}_#{born}"
         person.save
       end
+    end
+  end
+  
+  def load_from_file(file_name="members.js")
+    file = "data/#{file_name}"
+    
+    data = JSON.parse(File.read(file))
+    
+    data.each do |record|
+      person = Person.new
+      person.surname = record["surname"]
+      person.forenames = record["forenames"]
+      person.title = record["title"] if record["title"]
+      person.born = record["born"]
+      person.died = record["died"] if record["died"]
+      person.id = "#{person.surname}_#{person.forenames[0..0]}_#{person.born.year}"
+      person.save
     end
   end
 end
--- models/person.rb
+++ models/person.rb
@@ -1,12 +1,154 @@
+# encoding: utf-8
+
 require 'mongo_mapper'
 
 class Person
   include MongoMapper::Document
+  many :members, :in => :member_ids
   
   key :title, String
   key :forenames, String
   key :surname, String
+  key :aka, Array
   key :born, Date
   key :died, Date
   key :url, String
+  key :member_ids, Array
+  
+  def add_aka(name)
+    aka << name unless aka.include?(name)
+  end
+  
+  def self.find_all_by_name(name, year=nil)
+    parts = name.split
+    surname = parts.pop
+    forenames = parts.join(" ")
+    alt_forenames = nil
+    alt_surname = nil
+    
+    #look for a match
+    persons = find_people(name, /^#{surname}$/i, forenames)
+    
+    return persons unless persons.empty?
+    
+    #empty? see if part of the surname has been misidentified as a middle name
+    if parts.count > 1
+      parts = forenames.split(" ")
+      extra = parts.pop
+      alt_forenames = parts.join(" ")
+      alt_surname = "#{extra} #{surname}"
+      persons = find_people(name, /^#{alt_surname}$/i, alt_forenames)
+    end
+    
+    return persons unless persons.empty?
+    
+    #still empty? ok, look for known anomalies...
+    
+    #expected character encoding issues
+    surname = "Öpik" if surname == "Opik"
+    
+    #known spelling errors
+    if forenames == "Paul" and surname == "Farelly"
+      surname = "Farrelly"
+    end
+    if forenames == "Peter" and surname == "Horndern"
+      surname = "Hordern"
+    end
+    if forenames == "Irvine" and surname == "Patrick"
+      surname = "Patnick"
+    end
+    
+    #and oddities
+    if forenames == "William" and surname == "Smyth"
+      forenames = "Martin"
+    end
+    
+    #forename shortenings, lengthenings, etc...
+    forenames = forename_variations(forenames)
+    persons = find_people(name, /^#{surname}$/i, /#{forenames}/)
+    
+    if persons.empty? and alt_surname
+      forenames = forename_variations(alt_forenames)
+      persons = find_people(name, /^#{alt_surname}$/i, /#{forenames}/)
+    end
+    
+    persons
+  end
+  
+  private
+    def self.find_people(name, surname, forenames, year=nil)
+      persons = Person.find_all_by_surname_and_forenames(surname, forenames)
+      persons += Person.find_all_by_aka(name)
+      persons
+    end
+    
+    def self.forename_variations(forenames)
+      case forenames
+      when "Michael"        
+        forenames = forenames.gsub("Michael", "(?:Michael)|(?:Mick)|(?:Mike)")
+      when "Sion"
+        forenames = forenames.gsub("Sion", "Siôn")
+      when /(Ste(?:(?:ph)|v)en)/
+        forenames = forenames.gsub($1, "(?:Stephen)|(?:Steven)|(?:Steve)")
+      when "Llewellyn"
+        forenames = forenames.gsub("Llewellyn", "Llew")
+      when /((?:Christine)|(?:Christopher))/
+        forenames = forenames.gsub($1, "Chris")
+      when /(Phill?ip)/
+        forenames = forenames.gsub($1, "Phil")
+      when "Peter"
+        forenames = "Pete"
+      when "James"
+        forenames = "Jim"
+      when "Robert"
+        forenames = /(?:B|R)ob/
+      when "Jennifer"
+        forenames = "Jenny"
+      when "Andy"
+        forenames = "Andrew"
+      when /Anth?ony/
+        forenames = "Tony"
+      when "Lawrence"
+        forenames = "Lawrie"
+      when "Nicholas"
+        forenames = "Nick"
+      when "Thomas"
+        forenames = "Tom"
+      when "Marek"
+        forenames = "Mark"
+      when "Desmond"
+        forenames = "Des"
+      when "Raymond"
+        forenames = "Ray"
+      when "William"
+        forenames = /(?:W|B)ill/
+      when "Jeffrey"
+        forenames = "Jeff"
+      when "Archibald"
+        forenames = "Archie"
+      when "Elizabeth"
+        forenames = "Liz"
+      when "Edward"
+        forenames = /(?:Ed)|(?:Ted)/
+      when "Marjorie"
+        forenames = "Mo"
+      when "Timothy"
+        forenames = "Tim"
+      when "Joe"
+        forenames = "Joseph"
+      when "Alfred"
+        forenames = "Alf"
+      when "Ronald"
+        forenames = "Ron"
+      when "Stanley"
+        forenames = "Stan"
+      when "Terence"
+        forenames = "Terry"
+      when "Douglas"
+        forenames = "Doug"
+      when "Gregory"
+        forenames = "Greg"
+      end
+      forenames
+    end
 end
