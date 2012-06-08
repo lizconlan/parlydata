b3bdeebed3464a45c5df5e528235e67638983140
lizconlan <lizconlan@gmail.com>
2012-06-08 18:21:34 +0100

    linking election_results to members

--- models/election_result.rb
+++ models/election_result.rb
@@ -3,11 +3,12 @@ require 'mongo_mapper'
 class ElectionResult
   include MongoMapper::Document
   has_one :constituency
+  many :members, :in => :member_ids
   belongs_to :election
   
   key :_type, String
   key :election_id, BSON::ObjectId
   key :constituency_id, BSON::ObjectId
-  #key :member_id, BSON::ObjectId
   key :party, String
+  key :member_ids, Array
 end
