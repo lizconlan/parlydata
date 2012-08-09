e8eff117b9a10232a922554b4cb60bce3b58efb8
lizconlan <lizconlan@gmail.com>
2012-08-09 19:57:51 +0100

    slightly better appointments loader

--- lib/role_appointments_loader.rb
+++ lib/role_appointments_loader.rb
@@ -10,29 +10,36 @@ class RoleAppointmentsLoader
     response = RestClient.get(url)
     data = JSON.parse(response.body)
     data.each do |record|
-      if Date.parse(record["pm_from"]) > Date.parse("1992-04-08")
-        persons = Person.find_best_matches(record["name"])
+      if Date.parse(record["pm_from"]) > Date.parse("1978-01-01")
+        persons = Person.find_best_matches(record["name"].gsub(/^Sir /, ""))
         
+        p ""
         p "looking for #{record["name"]}"
         
         if persons.size > 1
           p "found multiple people :("
-        else
+        elsif persons.size == 1
           person = persons.first
+          p "found #{persons.first.aka[0]}"
+        else
+          p "#{record["name"]} not found :("
         end
         
-        appointment = RoleAppointment.new()
+        if person
+          appointment = RoleAppointment.new()
         
-        appointment.title = "Prime Minister"
-        appointment.appointed = record["pm_from"]
-        appointment.left_role = record["pm_to"] unless record["pm_to"] == "Incumbent"
-        appointment.person_id = person.id
+          appointment.title = "Prime Minister"
+          appointment.appointed = record["pm_from"]
+          appointment.left_role = record["pm_to"] unless record["pm_to"] == "Incumbent"
+          appointment.person_id = person.id
         
-        appointment.id = "Prime_Minister_#{appointment.appointed.year}-#{appointment.appointed.month}"
-        person.role_appointment_ids << appointment.id unless person.role_appointment_ids.include?(appointment.id)
+          appointment.id = "Prime_Minister_#{appointment.appointed.year}-#{appointment.appointed.month}"
+          person.role_appointment_ids << appointment.id unless person.role_appointment_ids.include?(appointment.id)
         
-        appointment.save
-        person.save
+          appointment.save
+          person.save
+        end
+        person = nil
       end
     end
   end
